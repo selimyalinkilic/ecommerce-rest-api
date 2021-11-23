66aaca1d37533473b7c39082377a74fdf2c2cc3b
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-24 00:28:25 +0300

    Created mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, () => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(8181, () => {
   console.log("Backend server is running at 8181!");
