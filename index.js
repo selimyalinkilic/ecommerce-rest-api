cad0f138edb1b2ac6a26a845ec94c32e63d3206c
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-27 01:51:27 +0300

    Added category and admin routes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const usersRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const adminRoute = require("./routes/admin");
+const categoryRoute = require("./routes/category");
 
 const app = express();
 
@@ -30,7 +32,12 @@ app.use(cors());
 
 // Routes
 app.use("/api/users", usersRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/category", categoryRoute);
 app.use("/api/auth", authRoute);
+app.get("/", (req, res) => {
+  res.send("Welcome to home page");
+});
 
 app.listen(8181, () => {
   console.log("Backend server is running at 8181!");
