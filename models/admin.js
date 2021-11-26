efd93571289345e1b889868962578936aeb39a41
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-27 01:53:02 +0300

    Created admin model

---
+++ models/admin.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const AdminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    surname: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    phone: {
+      type: String,
+    },
+    role: {
+      // Admin = 0, SuperAdmin = 1
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = Admin = mongoose.model("Admin", AdminSchema);
